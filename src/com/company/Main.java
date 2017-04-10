2a2bc4cd407e9f41e7400918d4bde91c0fbc3e80
Dlee99 <Dereklee9ab@aol.com>
2017-04-09 20:18:54 -0500

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,32 @@
+package com.company;
+
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandSender;
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class Main extends JavaPlugin {
+    public static void main(String[] args){}
+    
+    @Override
+    public void onEnable() {
+
+    }
+
+    @Override
+    public void onDisable() {
+
+    }
+
+    @Override
+    public boolean onCommand(CommandSender sender,
+                             Command command,
+                             String label,
+                             String[] args) {
+        if (command.getName().equalsIgnoreCase("mycommand")) {
+            sender.sendMessage("You ran /mycommand!");
+            return true;
+        }
+        return false;
+    }
+
+}
